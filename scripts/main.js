a9d052b339097d2620dc870e80a54f4023177d28
James Birchall <jamesbirchall@gmail.com>
2018-11-23 14:01:03 +0000

    js animation started

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // I want to clear this content, then progressively reveal the text over a few seconds...
     var intro = document.querySelector('.intro');
     intro.textContent = "";
+    console.log('DOM Loaded and Script run.');
 });
