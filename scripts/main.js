a8864ba52a1b75c0432aeba00e7eaa7a5182c0d8
James Birchall <jamesbirchall@gmail.com>
2018-11-23 14:22:17 +0000

    js animation started

--- scripts/main.js
+++ scripts/main.js
@@ -7,9 +7,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var options = {
         strings: ["Hello, I'm James", "Hello, I'm James &amp; I solve problems!", "Hello, I'm James &amp; I write Javascript!", "Hello, I'm James &amp; I write Python!", "Hello, I'm James &amp; I write SQL!"],
         typeSpeed: 30,
-        startDelay: 5000,
-        backDelay: 500,
-        loop: true
+        startDelay: 4000,
+        backDelay: 1000
     }
 
     var typed = new Typed(".intro", options);
