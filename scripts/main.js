97116b2de7b052766c148dff76eaa7c0b7c22c59
James Birchall <jamesbirchall@gmail.com>
2018-11-23 13:59:31 +0000

    js animation started

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,5 @@
-// Animation for the Hello Intro
-
-var intro = document.querySelector('.intro');
-
-// I want to clear this content, then progressively reveal the text over a few seconds...
-intro.textContent = "";
-
-var completionTime = 3.0;
+document.addEventListener("DOMContentLoaded", function(event) {
+    // I want to clear this content, then progressively reveal the text over a few seconds...
+    var intro = document.querySelector('.intro');
+    intro.textContent = "";
+});
