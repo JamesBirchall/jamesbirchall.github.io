a85568b8816c881d823b17018faaab2c25375f2d
James Birchall <jamesbirchall@gmail.com>
2018-11-26 11:58:38 +0000

    text update

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     intro.textContent = "";
 
     var options = {
-        strings: ["Hello, I'm James", "Hello, I'm James &amp; I make digital things", "Hello, I'm James &amp; I program in Python/JavaScript/SQL and more...", "Hello, I'm James &amp; I also like running!", "Hello, I'm James &amp; I solve problems, have you got one?"],
+        strings: ["Hello, I'm James", "Hello, I'm James &amp; I am a Developer & Data Analyst", "Hello, I'm James &amp; I spend time with Python, Javascript & SQL", "Hello, I'm James &amp; I also like running!", "Hello, I'm James &amp; I solve Digital problems, have you got one?"],
         typeSpeed: 30,
         startDelay: 2000,
         backDelay: 1000
