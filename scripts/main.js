7493dcccac399ca2f13ae453c4c3b746a1569da4
James Birchall <jamesbirchall@gmail.com>
2018-11-26 11:55:22 +0000

    timeing update start text

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var options = {
         strings: ["Hello, I'm James", "Hello, I'm James &amp; I make digital things", "Hello, I'm James &amp; I program in Python/JavaScript/SQL and more...", "Hello, I'm James &amp; I also like running!", "Hello, I'm James &amp; I solve problems, have you got one?"],
         typeSpeed: 30,
-        startDelay: 4000,
+        startDelay: 2000,
         backDelay: 1000
     }
 
