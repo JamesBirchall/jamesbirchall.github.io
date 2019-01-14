2f2f8bb4083bd0742fb80dc9b7f0f89d107d9349
James Birchall <jamesbirchall@gmail.com>
2019-01-14 13:31:26 +0000

    bug - cursor fix
    Fixing a cursor bug on the site

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
         strings: ["Hello, I'm James", "Hello, I'm James &amp; I'm Digital Analyst &amp; Developer", "Hello, I'm James &amp; I spend time with Adobe Analytics, Target, Google Analytics &amp; Tag Managers", "Hello, I'm James &amp; I spend time with JavaScript, SQL, Hadoop &amp; lots of Excel", "Hello, I'm James &amp; I also like running!", "Hello, I'm James &amp; I find answers, have you got a question?"],
         typeSpeed: 20,
         startDelay: 2000,
-        backDelay: 2000
+        backDelay: 2000,
+        showCursor: false
     }
 
     var typed = new Typed(".intro", options);
