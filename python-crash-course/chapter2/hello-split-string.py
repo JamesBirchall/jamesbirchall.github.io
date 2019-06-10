9dc435d66fab61aca748a41f1970b0b654bcaebd
James Birchall <jamesbirchall@gmail.com>
2019-06-10 12:25:24 +0000

    some more python

---
+++ python-crash-course/chapter2/hello-split-string.py
@@ -0,0 +1,10 @@
+"""
+    hello-split-string.py
+    Simple hello test program modified to use named varible string than direct
+    James Birchall 2019
+"""
+message = 'Hello Birchall!'
+print(message)
+
+message = 'Hello again in a different string.'
+print(message)
