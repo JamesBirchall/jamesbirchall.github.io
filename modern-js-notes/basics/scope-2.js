b17f789e4385e18804fe8a189f56ba73dded2ccb
James Birchall <jamesbirchall@gmail.com>
2019-06-15 08:12:40 +0100

    scope 2 finished
    finished the scope 2 file, looked at hoisting and scope check fo variables.

--- modern-js-notes/basics/scope-2.js
+++ modern-js-notes/basics/scope-2.js
@@ -1,16 +1,21 @@
 // scope-2.js
 // Birchall 2019
 
-let name = "Jimbo"
+//let name = 'Jimbo'
 
 if (true) {
     // can overwrite scope of parent scope variable
-    let name = "James"
+    //let name = 'James'
 
     if (true) {
+        name = 'Jones' // leaked global if set without let to scope
         console.log(name)
     }
 }
 
 if (true) {
+    console.log(name)
 }
+
+// if variable is used without declaring it's hoisted first to global scope
+// and accessible to whole program, each variable looks up scope hence why it's hoisted
