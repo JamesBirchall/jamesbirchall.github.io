97b96960d176c1cdd86f239b547ccde54a509d68
James Birchall <jamesbirchall@gmail.com>
2019-07-03 12:19:48 +0000

    grade-calculator js example

---
+++ modern-js-notes/functions/grade-calculator.js
@@ -0,0 +1,34 @@
+// grade-calculator.js
+// Birchall 2019
+
+// take in students score and total possible score e.g. (15, 20) 15 points out of 20 = 75%
+// print a message about the grade as a letter - case statement
+// 90-100 A, 80-89, B, 70-79 C, 60-69 D, 0-59 F
+
+let calculateGrade = function(percentageScore){
+    let grade = 'F'
+
+    if (percentageScore > 89) {
+        grade = 'A'
+    } else if (percentageScore > 79) {
+        grade = 'B'
+    } else if (percentageScore > 69) {
+        grade = 'C'
+    } else if (percentageScore > 59) {
+        grade = 'D'
+    }
+
+    return grade
+}
+
+let calculateScore = function(score, total){
+    return (100 / total) * score
+}
+
+let grades = [13,12,5,15,19,20,16]
+let totalScore = 20
+
+for(let i = 0; i < grades.length; i++) {
+    let percentageScore = calculateScore(grades[i], totalScore)
+    console.log(`Student ${i+1} got ${grades[i]} which is ${percentageScore}% and grade: ${calculateGrade(percentageScore)}`)
+}
