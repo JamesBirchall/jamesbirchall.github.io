527d59222ec27776d942a59348c8104ee79069fa
James Birchall <jamesbirchall@gmail.com>
2019-08-20 14:40:22 +0000

    finished string methods js

--- modern-js-notes/objects/string-methods.js
+++ modern-js-notes/objects/string-methods.js
@@ -25,3 +25,30 @@ console.log(longString.trim())
 
 // replace example using a regex which includesflags for repeat /g and ignore/i
 console.log(longString.replace(/ /gi, ''))
+
+// challenge
+
+let isValidPassword = function(potentialPassword) {
+    let valid = true
+    let listBannedWords = ['password', 'limes']
+
+    if (potentialPassword.length > 8) {
+        for (let element of listBannedWords) {
+            if(potentialPassword.toLocaleLowerCase().includes(element)) {
+                valid = false
+                console.log('Error: Bad password found, contains banned word')
+                break
+            }
+        }
+    } else {
+        valid = false
+        console.log('Error: Bad password found, too short')
+    }
+
+
+    return valid
+}
+
+console.log(isValidPassword('asdfel'))   // false
+console.log(isValidPassword('asd321321(*&^&^')) // true
+console.log(isValidPassword('asdasd*&^876876876password987324hjksdjfh')) // false
