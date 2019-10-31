a2d4c17ff0ba12c7993da6d4f4b13b5b4c7433cf
James Birchall <jamesbirchall@gmail.com>
2019-10-31 12:26:30 +0000

    continued dom related stuff

--- modern-js-notes/notes-app/scripts/main.js
+++ modern-js-notes/notes-app/scripts/main.js
@@ -4,6 +4,13 @@ header1.innerText = 'Something else'
 // DOM - Document Object Model manipulation
 
 // delete an element <p> tag
-const paragraph = document.querySelector('p')
-console.log(paragraph)
-paragraph.remove()
+// const paragraph = document.querySelector('p')   // only takes first instance of <p> querySelectorAll('') for array of all of them
+// console.log(paragraph)
+// paragraph.remove()
+
+// delete all element <p> tags
+const paragraphs = document.querySelectorAll('p')
+paragraphs.forEach(function(item) {
+    console.log(item)
+    item.remove()
+})
