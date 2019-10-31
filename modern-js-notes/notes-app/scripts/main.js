009b25532acb137312bcaa8f0613cd1809d231a6
James Birchall <jamesbirchall@gmail.com>
2019-10-31 12:15:51 +0000

    continued dom related stuff

--- modern-js-notes/notes-app/scripts/main.js
+++ modern-js-notes/notes-app/scripts/main.js
@@ -1,2 +1,8 @@
 let header1 = document.getElementById('header1')
 header1.innerText = 'Something else'
+
+// DOM - Document Object Model manipulation
+
+// delete an element <p> tag
+const paragraph = document.querySelector('p')
+console.log(`${paragraph} : Being Deleted...`)
