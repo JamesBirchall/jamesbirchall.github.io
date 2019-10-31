b4425e45bec20ecde75b3378d10eec96dea37328
James Birchall <jamesbirchall@gmail.com>
2019-10-31 13:50:53 +0000

    bug fix

--- modern-js-notes/todo-app/scripts/main.js
+++ modern-js-notes/todo-app/scripts/main.js
@@ -4,7 +4,7 @@
 // example of finding content and removing based on includes
 const paragrphs = document.querySelectorAll('p')
 paragrphs.forEach(function(item){
-    if(item.inndertext.includes('gym')){
+    if(item.innertext.includes('gym')){
         item.remove()
     }
 })
