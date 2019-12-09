8b788473acef7db76e22937e8c26c44d1e71bc4d
James Birchall <jamesbirchall@gmail.com>
2019-12-09 13:56:50 +0000

    event listener for select dropdow

--- modern-js-notes/notes-app/scripts/main.js
+++ modern-js-notes/notes-app/scripts/main.js
@@ -135,7 +135,7 @@ const renderNotes = function(notes, filters){
         console.log('sort by last-created')
         console.log(sortedNotes)
         sortedNotes = filteredNotes.sort(function (a, b) {
-            return a - b
+            return a.dateCreated.valueOf() - b.dateCreated.valueOf()
         })
         console.log(sortedNotes)
         break
