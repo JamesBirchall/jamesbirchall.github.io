f2fa3ab3c5421d73357f5bac7c044f19e9a7f001
James Birchall <jamesbirchall@gmail.com>
2020-02-24 13:49:56 +0000

    lastUpdate sync
    sync now when lastUpdate time is changed and multiple tabs open with the editor view of same item

--- modern-js-notes/notes-app/scripts/edit.js
+++ modern-js-notes/notes-app/scripts/edit.js
@@ -3,62 +3,59 @@
 
 const noteID = getNoteIDFromURLIDParameter()
 let notes = getNotesInLocalStorage()
-const titleElement = document.querySelector('#note-title')
-const bodyElement = document.querySelector('#note-body')
+const titleElement = document.querySelector("#note-title")
+const bodyElement = document.querySelector("#note-body")
 
 const index = findIndexOfExistingObject(noteID)
 
 if (index > -1) {
-    const note = notes[index]
-    titleElement.value = note.title
-    bodyElement.value = note.body
+  const note = notes[index]
+  titleElement.value = note.title
+  bodyElement.value = note.body
 
-    // set last edited value
-    const lastEditedSpan = document.createElement('span')
-    const lastUpdate = moment(note.updatedAt)
-    lastEditedSpan.appendChild(
-        document.createTextNode(`last edited: ${lastUpdate.toNow(true)}`)
-    )
-    document.querySelector('#last-update').append(lastEditedSpan)
+  generateLastUpdatedDOM(note)
 } else {
-    console.log('invalid ID, note not found')
-    console.log(`${noteID} not in list.`)
-    location.assign('./index.html')
+  console.log("invalid ID, note not found")
+  console.log(`${noteID} not in list.`)
+  location.assign("./index.html")
 }
 
-document.querySelector('#remove-note').addEventListener('click', function() {
-    deleteNote(noteID)
-    saveNotesToLocalStorage()
-    location.assign('./index.html')
+document.querySelector("#remove-note").addEventListener("click", function() {
+  deleteNote(noteID)
+  saveNotesToLocalStorage()
+  location.assign("./index.html")
 })
 
-titleElement.addEventListener('input', function(e) {
-    const note = notes[index]
-    note.title = e.target.value
-    note.updatedAt = Date.now()
-    saveNotesToLocalStorage()
+titleElement.addEventListener("input", function(e) {
+  const note = notes[index]
+  note.title = e.target.value
+  note.updatedAt = Date.now()
+  generateLastUpdatedDOM(note)
+  saveNotesToLocalStorage()
 })
 
-bodyElement.addEventListener('input', function(e) {
-    const note = notes[index]
-    note.body = e.target.value
-    note.updatedAt = Date.now()
-    saveNotesToLocalStorage()
+bodyElement.addEventListener("input", function(e) {
+  const note = notes[index]
+  note.body = e.target.value
+  note.updatedAt = Date.now()
+  generateLastUpdatedDOM(note)
+  saveNotesToLocalStorage()
 })
 
-window.addEventListener('storage', function(e) {
-    if (e.key === 'notes') {
-        notes = JSON.parse(e.newValue)
-        const index = findIndexOfExistingObject(noteID)
+window.addEventListener("storage", function(e) {
+  if (e.key === "notes") {
+    notes = JSON.parse(e.newValue)
+    const index = findIndexOfExistingObject(noteID)
 
-        if (index > -1) {
-            const note = notes[index]
-            titleElement.value = note.title
-            bodyElement.value = note.body
-        } else {
-            console.log('invalid ID, note not found')
-            console.log(`${noteID} not in list.`)
-            location.assign('./index.html')
-        }
+    if (index > -1) {
+      const note = notes[index]
+      titleElement.value = note.title
+      bodyElement.value = note.body
+      generateLastUpdatedDOM(note)
+    } else {
+      console.log("invalid ID, note not found")
+      console.log(`${noteID} not in list.`)
+      location.assign("./index.html")
     }
+  }
 })
--- modern-js-notes/notes-app/scripts/notes-functions.js
+++ modern-js-notes/notes-app/scripts/notes-functions.js
@@ -4,50 +4,67 @@
 
 // check for existing notes saved in localStorage and return string
 function getNotesInLocalStorage() {
-    const notesJSON = localStorage.getItem('notes')
-    if (notesJSON) {
-        return JSON.parse(notesJSON)
-    } else {
-        return []
-    }
+  const notesJSON = localStorage.getItem("notes")
+  if (notesJSON) {
+    return JSON.parse(notesJSON)
+  } else {
+    return []
+  }
 }
 
 // save notes to storage
 function saveNotesToLocalStorage() {
-    localStorage.setItem('notes', JSON.stringify(notes))
+  localStorage.setItem("notes", JSON.stringify(notes))
 }
 
 // remove notes from storage
 function removeNotesFromLocalStorage() {
-    localStorage.removeItem('notes')
+  localStorage.removeItem("notes")
 }
 
 function clearNotesArray() {
-    window.notes = [] // reset notes array and future note count
-    removeNotesFromLocalStorage()
+  window.notes = [] // reset notes array and future note count
+  removeNotesFromLocalStorage()
 }
 
 const deleteNote = function(id) {
-    const notesIndex = findIndexOfExistingObject(id)
+  const notesIndex = findIndexOfExistingObject(id)
 
-    if (notesIndex > -1) {
-        notes.splice(notesIndex, 1)
-    }
+  if (notesIndex > -1) {
+    notes.splice(notesIndex, 1)
+  }
 }
 
 const findIndexOfExistingObject = function(id) {
-    const notesIndex = notes.findIndex(function(note) {
-        return note.id === id
-    })
+  const notesIndex = notes.findIndex(function(note) {
+    return note.id === id
+  })
 
-    return notesIndex
+  return notesIndex
 }
 
 const getNoteIDFromURLIDParameter = function() {
-    const idString = location.href.split('#')[1]
+  const idString = location.href.split("#")[1]
 
-    if (idString.startsWith('id=')) {
-        const id = idString.split('=')[1]
-        return id
-    }
+  if (idString.startsWith("id=")) {
+    const id = idString.split("=")[1]
+    return id
+  }
+}
+
+const generateLastUpdatedDOM = function(note) {
+  const lastEditedSpan = document.querySelector("#last-updated-span")
+  if (lastEditedSpan) {
+    const lastUpdate = moment(note.updatedAt)
+    lastEditedSpan.textContent = `last edited: ${lastUpdate.toNow(true)}`
+  } else {
+    // set last edited value
+    const lastEditedSpan = document.createElement("span")
+    lastEditedSpan.setAttribute("id", "last-updated-span")
+    const lastUpdate = moment(note.updatedAt)
+    lastEditedSpan.appendChild(
+      document.createTextNode(`last edited: ${lastUpdate.toNow(true)}`)
+    )
+    document.querySelector("#last-update").append(lastEditedSpan)
+  }
 }
