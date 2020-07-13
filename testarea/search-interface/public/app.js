49b747299c69ce0d578744a8bf54cf2ccec84cab
James Birchall <jamesbirchall@gmail.com>
2020-07-13 11:58:56 +0100

    test app
    test app

---
+++ testarea/search-interface/public/app.js
@@ -0,0 +1,72 @@
+// Update the client ID and search app ID for your deployment.
+// [START cloud_search_github_tutorial_search_config]
+let searchConfig = {
+  //clientId: "943104600333-b0rruptgea41rlc5gk9aocrgeugk17io.apps.googleusercontent.com",
+  clientId:
+    "556284081694-99jq9qge7noregh18vl6cia0pcumb2sv.apps.googleusercontent.com",
+  searchAppId: "searchapplications/22d28475e6f3962fe52fca3916f4eb9a",
+}
+// [END cloud_search_github_tutorial_search_config]
+
+// Reference to the results container to clear results on login/logout changes.
+let resultsContainer
+
+// [START cloud_search_tutorial_on_load]
+/**
+ * Load the cloud search widget & auth libraries. Runs after
+ * the initial gapi bootstrap library is ready.
+ */
+function onLoad() {
+  gapi.load("client:auth2:cloudsearch-widget", initializeApp)
+}
+// [END cloud_search_tutorial_on_load]
+
+// [START cloud_search_tutorial_init_app]
+/**
+ * Initialize the app after loading the Google API client &
+ * Cloud Search widget.
+ */
+async function initializeApp() {
+  await gapi.auth2.init({
+    clientId: searchConfig.clientId,
+    scope: "https://www.googleapis.com/auth/cloud_search.query",
+  })
+
+  // [START cloud_search_tutorial_init_auth]
+  let auth = gapi.auth2.getAuthInstance()
+
+  // Watch for sign in status changes to update the UI appropriately
+  let onSignInChanged = (isSignedIn) => {
+    document.getElementById("app").hidden = !isSignedIn
+    document.getElementById("welcome").hidden = isSignedIn
+    if (resultsContainer) {
+      resultsContainer.clear()
+    }
+  }
+  auth.isSignedIn.listen(onSignInChanged)
+  onSignInChanged(auth.isSignedIn.get()) // Trigger with current status
+
+  // Connect sign-in/sign-out buttons
+  document.getElementById("sign-in").onclick = (e) => auth.signIn()
+  document.getElementById("sign-out").onclick = (e) => auth.signOut()
+  // [END cloud_search_tutorial_init_auth]
+
+  // [START_EXCLUDE]
+  // [START cloud_search_tutorial_init_widget]
+  gapi.config.update("cloudsearch.config/apiVersion", "v1")
+  resultsContainer = new gapi.cloudsearch.widget.resultscontainer.Builder()
+    .setSearchApplicationId(searchConfig.searchAppId)
+    .setSearchResultsContainerElement(document.getElementById("search_results"))
+    .setFacetResultsContainerElement(document.getElementById("facet_results"))
+    .build()
+
+  const searchBox = new gapi.cloudsearch.widget.searchbox.Builder()
+    .setSearchApplicationId(searchConfig.searchAppId)
+    .setInput(document.getElementById("search_input"))
+    .setAnchor(document.getElementById("suggestions_anchor"))
+    .setResultsContainer(resultsContainer)
+    .build()
+  // [END cloud_search_tutorial_init_widget]
+  // [END_EXCLUDE]
+}
+// [END cloud_search_tutorial_init_app]
