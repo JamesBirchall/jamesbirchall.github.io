0c015fe26835a3cc3c6c38481c62c596c9ff4606
James Birchall <jamesbirchall@gmail.com>
2020-08-03 09:30:20 +0100

    improvements
    improvements - feedback to user & reloading automatically when completed

--- modern-js-notes/hangman/app.js
+++ modern-js-notes/hangman/app.js
@@ -6,16 +6,22 @@ const puzzleElement = document.querySelector("#puzzle")
 const guessesElement = document.querySelector("#guesses")
 const hangmanRound1 = new Hangman("HigH hOrse", 7)
 puzzleElement.textContent = hangmanRound1.getPuzzle()
-guessesElement.textContent = hangmanRound1.guesses
+guessesElement.textContent = hangmanRound1.statusMessage()
 
 window.addEventListener("keypress", function (e) {
-  // console.log(e.key)
   checkEntry(e.key)
 })
 
 const checkEntry = function (key) {
-  console.log(hangmanRound1.getPuzzle())
   hangmanRound1.makeGuess(key)
   puzzleElement.textContent = hangmanRound1.getPuzzle()
-  guessesElement.textContent = hangmanRound1.guesses
+  guessesElement.textContent = hangmanRound1.statusMessage()
+}
+
+async function reloadGame() {
+  // wait for 2 seconds
+  await new Promise((r) => setTimeout(r, 2000))
+
+  // reload the page
+  window.location.reload()
 }
--- modern-js-notes/hangman/hangman.js
+++ modern-js-notes/hangman/hangman.js
@@ -7,6 +7,7 @@ const Hangman = function (word, guesses) {
   this.word = word.toLowerCase().split("")
   this.guesses = guesses
   this.guessedLetters = []
+  this.status = "playing"
 }
 
 Hangman.prototype.getPuzzle = function () {
@@ -24,7 +25,41 @@ Hangman.prototype.getPuzzle = function () {
   return puzzle
 }
 
+Hangman.prototype.calculateStatus = function () {
+  // if we have no guesses left then game over!
+  if (this.guesses <= 0) {
+    this.status = "failed"
+    reloadGame()
+  } else {
+    // check each letter has been matched, count letters and white spaces and combine to check if finished as well
+    let finished = false
+    let foundLetters = 0
+    let foundSpaces = 0
+
+    for (let i = 0; i < this.word.length; i++) {
+      if (this.guessedLetters.includes(this.word[i])) {
+        foundLetters++
+      } else if (this.word[i] === " ") {
+        foundSpaces++
+      }
+    }
+
+    if (this.word.length === foundLetters + foundSpaces) {
+      finished = true
+    }
+
+    if (finished) {
+      this.status = "finished"
+      reloadGame()
+    }
+  }
+}
+
 Hangman.prototype.makeGuess = function (guess) {
+  if (this.status !== "playing") {
+    return
+  }
+
   guess = guess.toLowerCase()
 
   const isUnique = !this.guessedLetters.includes(guess)
@@ -37,4 +72,16 @@ Hangman.prototype.makeGuess = function (guess) {
   if (isUnique && isBadGuess) {
     this.guesses--
   }
+
+  this.calculateStatus()
+}
+
+Hangman.prototype.statusMessage = function () {
+  if (this.status === "failed") {
+    return `Nice try! The word was: ${this.word.join("")}`
+  } else if (this.status === "finished") {
+    return "Great Work! You guessed the word correctly :-)"
+  }
+
+  return `Guesses Left: ${this.guesses}`
 }
