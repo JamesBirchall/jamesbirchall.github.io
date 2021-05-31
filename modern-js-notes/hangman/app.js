59cb4cdfaf391e80503df3a479f2bd166ba599de
James Birchall <jamesbirchall@gmail.com>
2021-05-31 21:27:46 +0100

    api request examples
    Added some API reuqest examples.

--- modern-js-notes/hangman/app.js
+++ modern-js-notes/hangman/app.js
@@ -33,14 +33,42 @@ async function reloadGame() {
 }
 
 // Making a HTTP Request
-const request = new XMLHttpRequest()
+const request = new XMLHttpRequest()  // constructor function
 request.addEventListener("readystatechange", function (e) {
-  if(e.target.readyState === 4){
+  if(e.target.readyState === 4 && e.target.status === 200){
     // console.log(e.target)
+    // console.log(e) // prints the readystatechange object
+    // console.log(e.target.status + ': '+ e.target.statusText)
     const data = JSON.parse(e.target.responseText)
     console.log(data['puzzle'])
+  } else if(e.target.readyState === 4){
+    console.log('Error occured, unable to fetch data: ' + e.target.status + ': ' + e.target.statusText)
   }
 })
 
 request.open("GET", "http://puzzle.mead.io/puzzle")
 request.send()  // initiate the process to connect/process/return response
+
+// example using restcountries.eu api and the find function
+
+const countryCode = 'GB'
+const countryRequest = new XMLHttpRequest()
+countryRequest.addEventListener('readystatechange', (e) => {
+  if(e.target.readyState === 4 && e.target.status === 200){
+    const data = JSON.parse(e.target.responseText)
+    // Find function takes a function that contains the element of an array and a check to return the find object it matches
+    // in this case we are checking for the first object in the array whose value for the key alpha2Code matches GB
+    const foundCountry = data.find((country) => country.alpha2Code === countryCode)
+    // console.log(data)
+    if(foundCountry){
+      // console.log(foundCountry)
+      console.log(foundCountry.name)
+    }
+  } else if(e.target.readyState === 4){
+    console.log(e)
+    console.log('Error occured, unable to fetch data: ' + e.target.status + ': ' + e.target.statusText)
+  }
+})
+
+countryRequest.open('GET','https:restcountries.eu/rest/v2/all')
+countryRequest.send()
