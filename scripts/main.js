e7184c1e3cd5f8cac79c1f1688be6c21015a8013
James Birchall <jamesbirchall@gmail.com>
2023-08-31 12:27:06 +0100

    update page script
    update hp script

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,22 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
+  // Clear text if JS is present so animation can run
+  var intro = document.querySelector(".intro")
+  intro.textContent = ""
 
-    // Clear text if JS is present so animation can run
-    var intro = document.querySelector('.intro');
-    intro.textContent = "";
+  var options = {
+    strings: [
+      "Hello, I'm James",
+      "Hello, I'm James &amp; I'm a Google Cloud Certified Architect &amp; Engineer",
+      "Hello, I'm James &amp; I spend time with all things Cloud, Data &amp; Marketing related",
+      "Hello, I'm James &amp; I spend time with JavaScript, SQL, &amp; lots of Python &amp; PowerPoint",
+      "Hello, I'm James &amp; I also like running!",
+      "Hello, I'm James &amp; I find digital solutions, have you got a digital problem?",
+    ],
+    typeSpeed: 20,
+    startDelay: 2000,
+    backDelay: 2000,
+    showCursor: false,
+  }
 
-    var options = {
-        strings: ["Hello, I'm James", "Hello, I'm James &amp; I'm a Digital Analyst &amp; Developer", "Hello, I'm James &amp; I spend time with Adobe Analytics, Google Analytics &amp; Tag Managers", "Hello, I'm James &amp; I spend time with JavaScript, SQL, &amp; lots of Excel &amp; PowerPoint", "Hello, I'm James &amp; I also like running!", "Hello, I'm James &amp; I find answers, have you got a question?"],
-        typeSpeed: 20,
-        startDelay: 2000,
-        backDelay: 2000,
-        showCursor: false
-    }
-
-    var typed = new Typed(".intro", options);
-});
+  var typed = new Typed(".intro", options)
+})
